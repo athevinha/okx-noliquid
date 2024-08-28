f4b1c00cbbe8b839d8f6920c78d7626715db7223
athevinha <nguyenthevinh1002@gmail.com>
2024-08-28 16:11:50 +0700

    some change

--- bot/backtest/index.ts
+++ bot/backtest/index.ts
@@ -1,10 +1,14 @@
 import {getAccountPositions} from "../helper/okx-account";
 import {getSymbolCandles} from "../helper/okx-candles";
 import {findEMACrossovers, simulateTrades} from "../signals/ema-cross";
-import {decodeTimestamp} from "../utils";
+import {decodeTimestamp, decodeTimestampAgo, zerofy} from "../utils";
 import {WHITE_LIST_TOKENS_TRADE} from "../utils/config";
 
 const main = async () => {
+  let estTotalPnl = 0
+  let startTime = 0
+  let totalVolume = 0
+  const FEE = 0.02 // open & close
   await Promise.all(
     WHITE_LIST_TOKENS_TRADE.map(async (SYMBOL) => {
       const candles = await getSymbolCandles({
@@ -13,28 +17,41 @@ const main = async () => {
         bar: "15m",
         limit: 10000,
       });
-      console.log(SYMBOL, candles.length)
-      // const emaCross = findEMACrossovers(candles, 9, 21);
-      // console.log(emaCross.slice(-3).map(e => {
-      //   return {
-      //     ...e,
-      //     ts: decodeTimestamp(e.ts)
-      //   }
-      // }))
-      // const trades = simulateTrades(emaCross, 500);
-      // console.log(
-      //   `-----------------------${SYMBOL}-----------------------------`
-      // );
-      // console.log("Total PNL ($):", trades.totalPnL);
-      // console.log("Volume ($):", trades.totalVolumeInUSD);
-      // console.log("Total Tx:", trades.totalTransactions);
-      // console.log(
-      //   "Trade time:",
-      //   trades.closedTrades[0].ts,
-      //   "->",
-      //   trades.closedTrades[trades.closedTrades.length - 1].ts
-      // );
+
+      const emaCross = findEMACrossovers(candles, 9, 21);
+      console.log(emaCross.slice(-3).map(e => {
+        return {
+          ...e,
+          ts: decodeTimestamp(e.ts)
+        }
+      }))
+      const trades = simulateTrades(emaCross, 500);
+      console.log(
+        `-----------------------${SYMBOL}-----------------------------`
+      );
+      console.log("Total PNL ($):", trades.totalPnL);
+      estTotalPnl += trades.totalPnL
+      console.log("Volume ($):", trades.totalVolumeInUSD);
+      console.log("Total Tx:", trades.totalTransactions);
+      startTime = Number(candles[0].ts)
+      totalVolume += trades.totalVolumeInUSD
+
+      console.log(
+        "Trade time:",
+        trades.closedTrades[0].ts,
+        "->",
+        trades.closedTrades[trades.closedTrades.length - 1].ts
+      );
     })
   );
+  console.log(
+    `----------------------------------------------------`
+  );
+  console.log("Est. Trade time:", decodeTimestampAgo(startTime));
+  console.log("Est. Volume ($):", zerofy(totalVolume));
+  console.log("Est. Fee ($):", zerofy(totalVolume * FEE / 100));
+  console.log("Est. All total Pnl ($):", zerofy(estTotalPnl));
+  console.log("Est. All realize Pnl ($):", zerofy(estTotalPnl - (totalVolume * FEE / 100)));
+
 }
 main()
--- bot/utils/index.ts
+++ bot/utils/index.ts
@@ -4,29 +4,30 @@ export function decodeTimestamp(ts: number, UTC: number =  7 * 60 * 60 * 1000): 
     return date.toISOString(); // Returns date in ISO 8601 format (e.g., '2023-08-23T12:34:56.789Z')
 }
 export function decodeTimestampAgo(timestamp: number, clean = false): string {
-    const currentTime = Date.now();
-    const difference = currentTime - timestamp;
+  const currentTime = Date.now();
+  const difference = currentTime - timestamp;
 
+  const millisecondsPerMinute = 60 * 1000;
+  const millisecondsPerHour = 60 * millisecondsPerMinute;
+  const millisecondsPerDay = 24 * millisecondsPerHour;
 
-    const millisecondsPerMinute = 60 * 1000;
-    const millisecondsPerHour = 60 * millisecondsPerMinute;
+  const days = Math.floor(difference / millisecondsPerDay);
+  const hours = Math.floor((difference % millisecondsPerDay) / millisecondsPerHour);
+  const minutes = Math.floor((difference % millisecondsPerHour) / millisecondsPerMinute);
 
+  const daysText = days > 0 ? `${days}${clean ? 'd' : ` day${days !== 1 ? "s" : ""}`}` : "";
+  const hoursText = hours > 0 ? `${hours}${clean ? 'h' : ` hour${hours !== 1 ? "s" : ""}`}` : "";
+  const minutesText = minutes > 0 ? `${minutes}${clean ? 'm' : ` min${minutes !== 1 ? "s" : ""}`}` : "";
 
-    const hours = Math.floor(difference / millisecondsPerHour);
-    const minutes = Math.floor(
-        (difference % millisecondsPerHour) / millisecondsPerMinute
-    );
-    const hoursText = hours > 0 ? `${hours}${clean ? 'h' : ` hour${hours !== 1 ? "s" : ""}`}` : "";
-    const minutesText =
-        minutes > 0 ? `${minutes}${clean ? 'm' : ` min${minutes !== 1 ? "s" : ""}`}` : "";
-
-
-    if (hours > 0 && minutes > 0) {
-        return `${hoursText} ${minutesText} ago`;
-    } else {
-        return `${hoursText}${minutesText} ago`;
-    }
+  if (days > 0) {
+      return `${daysText} ${hoursText} ago`;
+  } else if (hours > 0) {
+      return `${hoursText} ${minutesText} ago`;
+  } else {
+      return `${minutesText} ago`;
+  }
 }
+
 export function createSignature(timestamp: string, method: string, requestPath: string, body?: string, SECRET_KEY?: string): string {
     const prehashString = `${timestamp}${method}${requestPath}${body}`;
     const hmac = crypto.createHmac('sha256', SECRET_KEY || '');
