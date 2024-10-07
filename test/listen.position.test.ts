d107bb074ac99905e944759216b278c1126d8ad6
athevinha <nguyenthevinh1002@gmail.com>
2024-10-07 22:56:52 +0700

    position socket test

--- test/listen.position.test.ts
+++ test/listen.position.test.ts
@@ -1,83 +1,66 @@
-import { setTimeout } from "timers/promises";
-import { expect } from "chai";
-import { wsPositions, wsTicks } from "../bot/helper/okx.socket";
-import { getSymbolCandles } from "../bot/helper/okx.candles";
-import { calculateATR } from "../bot/signals/atr";
-import { makeWsAuth } from "../bot/helper/auth";
-import { getAccountPendingAlgoOrders } from "../bot/helper/okx.account";
-import { IPositionOpen } from "../bot/type";
+import {expect} from "chai";
+import {setTimeout} from "timers/promises";
+import {
+  getAccountOrder
+} from "../bot/helper/okx.account";
+import {wsPositions} from "../bot/helper/okx.socket";
+import {closeFuturePosition,openFuturePosition} from "../bot/helper/okx.trade";
+import {ImgnMode,IPosSide} from "../bot/type";
+import {decodeTag} from "../bot/utils";
 
-// describe("OKX socket test ticker", () => {
-//   const { instID, bar } = {
-//     bar: "1m",
-//     instID: "BTC-USDT-SWAP",
-//   };
-//   it("OKX new candles socket test", async () => {
-
-//     ws.close();
-//   });
-// });
-
-const calculateATRWithWs = async ({instIds, bar}: {instIds:string[], bar:string}) => {
-
-  let candles = await getSymbolCandles({
-    instID: instIds[0],
-    bar,
-    before: 0,
-    limit: 3000,
-  });
-  const atrs = calculateATR(candles, 14);
-  let currentATR = atrs[atrs.length - 1].atr;
-  
-  const ws = wsTicks({
-    subscribeMessage: {
-      op: "subscribe",
-      args: instIds.map((instId) => ({
-          channel: 'mark-price',
-          instId: instId,
-        }))
-    },
-    subcribedCallBack(param) {console.log(param)},
-    messageCallBack(mark) {
-      const markPrice = Number(mark.data[0].markPx);
-      if(markPrice < candles[candles.length - 1].l) candles[candles.length - 1].l = markPrice
-      if(markPrice > candles[candles.length - 1].h) candles[candles.length - 1].h = markPrice
-      candles[candles.length - 1].c = markPrice
-      currentATR =  calculateATR(candles, 14).slice(-1)[0].atr
-    },
-    errorCallBack(e) {console.log(e)},
-  });
-}
-
-const trailingWithWs = async (openPos: IPositionOpen[]) => {
-  try {
-    const algoOrders = await getAccountPendingAlgoOrders({});
-    openPos.filter((pos) => {
-      // const isClosed = !pos.avgPx;
-      const algoOrder = algoOrders.filter(
-        (aOrd) => aOrd.instId === pos.instId
-      )?.[0];
-      if (algoOrder?.moveTriggerPx || algoOrder?.callbackRatio) return false; // Already set a trailing loss orders
-      return true
+describe("OKX positions with test", () => {
+  const TEST_CONFIG = {
+    instId: "PEPE-USDT-SWAP",
+    leverage: 5,
+    size: 100,
+    mgnMode: "isolated",
+    posSide: "long" as IPosSide,
+    campaignId: "test" + Math.random().toFixed(4).replaceAll(".", ""),
+  };
+  let ordId = "";
+  const { campaignId, instId, mgnMode, size, posSide, leverage } = TEST_CONFIG;
+  const tag = decodeTag({ campaignId, instId, leverage, posSide, size });
+  it("open position OKX", async () => {
+    const { openAlgoOrderRes, openPositionRes } = await openFuturePosition({
+      campaignId,
+      instId: instId,
+      size: size,
+      mgnMode: mgnMode as ImgnMode,
+      posSide: posSide as IPosSide,
+      leverage: leverage,
     });
-    calculateATRWithWs({instIds: openPos.map(pos => pos.instId), bar: '1H'})
-  } catch (error) {
-    console.log(error);
-  }
-};
+    expect(openAlgoOrderRes.msg).eq("");
+    expect(openPositionRes.msg).eq("");
 
-const ws = wsPositions({
-  authCallBack(config) {
-    console.log(config);
-  },
-  subcribedCallBack(param) {
-    console.log(param);
-  },
-  messageCallBack(pos) {
-    console.log("------------------------");
-    trailingWithWs(pos.data);
-  },
-  errorCallBack(e) {
-    console.log(e);
-  },
+    ordId = openPositionRes.data[0].ordId;
+    const order = await getAccountOrder({ instId, ordId });
+    expect(order.length).greaterThan(0);
+    await setTimeout(1000);
+  });
+    it("OKX new candles socket test", async () => {
+      const ws = wsPositions({
+        authCallBack(config) {
+        },
+        subcribedCallBack(param) {
+        },
+        messageCallBack(pos) {
+          expect(pos.data.length).greaterThan(0)
+        },
+        errorCallBack(e) {
+        },
+      });
+      await setTimeout(3000)
+      ws.close();
+    });
+  it("Close position OKX", async () => {
+    const { closeAlgoOrderRes, closePositionRes } = await closeFuturePosition({
+      instId: instId,
+      mgnMode: mgnMode as ImgnMode,
+      posSide: posSide as IPosSide,
+      tag,
+      isCloseAlgoOrders: false,
+    });
+    expect(closeAlgoOrderRes.msg).eq("");
+    expect(closePositionRes.msg).eq("");
+  });
 });
